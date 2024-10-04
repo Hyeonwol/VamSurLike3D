9f692d00aa935c3412a126dc92d892acbead8b72
Hyeonwol <cys8690@naver.com>
2024-10-04 21:43:16 +0900

    feat: 적 캐릭터 추적 구현

--- Source/VamSurLike3D/Public/MainCharacter/VS3DCharacter.h
+++ Source/VamSurLike3D/Public/MainCharacter/VS3DCharacter.h
@@ -6,14 +6,11 @@
 #include "GameFramework/Character.h"
 #include "EnhancedInputSubsystems.h"
 #include "AbilitySystemInterface.h"
-#include "AttributeSet.h"
-
 #include "VS3DCharacter.generated.h"
 
 class UCameraComponent;
 class USpringArmComponent;
 class UCharacterAttributeSet;
-
 
 UCLASS()
 class VAMSURLIKE3D_API AVS3DCharacter : public ACharacter, public IAbilitySystemInterface
@@ -22,21 +19,19 @@ class VAMSURLIKE3D_API AVS3DCharacter : public ACharacter, public IAbilitySystem
 
 public:
 	AVS3DCharacter();
-	
 	virtual void Tick(float DeltaTime) override;
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
 	virtual void BeginPlay() override;
-
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
 public:
-
-	//Input Action & Input Mapping Context
+	//Enhanced Input 관련
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input)
-	UInputMappingContext* CharacterMappingContext;
-
+	TObjectPtr<UInputMappingContext> CharacterMappingContext;
+	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input)
-	UInputAction* MoveAction;
+	TObjectPtr<UInputAction> MoveAction;
 
 	//움직임 관련 함수
 	void Move(const FInputActionValue& Value);
@@ -48,6 +43,10 @@ public:
 	UPROPERTY(EditAnywhere)
 	TObjectPtr<UCameraComponent> FollowCamera;
 
+	//등장 애니메이션 재생 중인지 확인. 등장 애니메이션이 재생 중이라면, 이동 불가, 공격 애니메이션 재생 X
+	UPROPERTY(BlueprintReadWrite, Category = "Animation")
+	bool bIsPlayingIntroAnimation = true;
+	
 	//GAS 관련
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Abilities")
 	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
